4cc5f07cd065e98e6f0d4f80a47f352f6fde425e
hellory4n <hellory4n@gmail.com>
2025-02-18 18:45:27 -0500

    YIP[EEEEEEEEEE

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,11 +1,13 @@
-print(starry.deg2rad(180))
-print(starry.rad2deg(3.14))
-print(starry.random_base64(10))
-print(starry.clamp(56934, 0, 10))
+function main()
+    print(starry.deg2rad(180))
+    print(starry.rad2deg(3.14))
+    print(starry.random_base64(10))
+    print(starry.clamp(56934, 0, 10))
 
-print(vec2.new(5, 6).x)
-print(vec2.new(5, 6).y)
+    local mate = vec2.new(5, 6)
+    print(mate)
+end
 
-local mate = vec2.new(1, 2)
-print(mate.x)
-print(mate.y)
+-- just so we get a stack trace
+local _, err = xpcall(main, debug.traceback);
+if err ~= nil then print(err) end
--- src/modules/extensions/extensions.c
+++ src/modules/extensions/extensions.c
@@ -48,6 +48,24 @@ static int32 starry_clamp(lua_State* lua)
     return 1;
 }
 
+static int32 starry_vec2_new(lua_State* lua)
+{
+    float64 x = luaL_checknumber(lua, 1);
+    float64 y = luaL_checknumber(lua, 2);
+
+    lua_newtable(lua);
+    luaL_getmetatable(lua, "vec2");
+    lua_setmetatable(lua, -2);
+
+    // set x and y
+    lua_pushnumber(lua, x);
+    lua_setfield(lua, -2, "x");
+    lua_pushnumber(lua, y);
+    lua_setfield(lua, -2, "y");
+
+    return 1;
+}
+
 static int32 starry_vec2_add(lua_State* lua);
 static int32 starry_vec2_sub(lua_State* lua);
 static int32 starry_vec2_mul(lua_State* lua);
@@ -57,56 +75,17 @@ static int32 starry_vec2_unm(lua_State* lua);
 static int32 starry_vec2_eq(lua_State* lua);
 static int32 starry_vec2_lt(lua_State* lua);
 static int32 starry_vec2_lte(lua_State* lua);
-static int32 starry_vec2_str(lua_State* lua);
 
-static int32 starry_vec2_new(lua_State* lua)
-{
-    // parameters
-    float64 x = luaL_checknumber(lua, 1);
-    float64 y = luaL_checknumber(lua, 2);
-
-    // man
-    // 2 for the elements, 11 for the metatable methods
-    lua_createtable(lua, 0, 13);
-
-    lua_pushnumber(lua, x);
-    lua_setfield(lua, -2, "x");
-
-    lua_pushnumber(lua, y);
-    lua_setfield(lua, -2, "y");
-    
-    lua_pushcfunction(lua, starry_vec2_add);
-    lua_setfield(lua, -2, "__add");
-    return 1;
-}
-
-static int32 starry_vec2_add(lua_State* lua)
+static int32 starry_vec2_str(lua_State* lua)
 {
     lua_getfield(lua, 1, "x");
-    float64 x1 = luaL_checknumber(lua, 1);
+    float64 x = lua_tonumber(lua, -1);
     lua_getfield(lua, 1, "y");
-    float64 y1 = luaL_checknumber(lua, 2);
+    float64 y = lua_tonumber(lua, -1);
 
-    lua_getfield(lua, 2, "x");
-    float64 x2 = luaL_checknumber(lua, 1);
-    lua_getfield(lua, 2, "y");
-    float64 y2 = luaL_checknumber(lua, 2);
-
-    lua_pushnumber(lua, x1 + x2);
-    lua_pushnumber(lua, y1 + y2);
-    starry_vec2_new(lua);
+    lua_pushfstring(lua, "vec2(%f, %f)", x, y);
     return 1;
 }
-
-static int32 starry_vec2_sub(lua_State* lua);
-static int32 starry_vec2_mul(lua_State* lua);
-static int32 starry_vec2_div(lua_State* lua);
-static int32 starry_vec2_mod(lua_State* lua);
-static int32 starry_vec2_unm(lua_State* lua);
-static int32 starry_vec2_eq(lua_State* lua);
-static int32 starry_vec2_lt(lua_State* lua);
-static int32 starry_vec2_lte(lua_State* lua);
-static int32 starry_vec2_str(lua_State* lua);
 
 /// binds every function ever
 static void bind()
@@ -129,10 +108,26 @@ static void bind()
     // global functions are accessed through the starry table
     lua_setglobal(lua, "starry");
 
-    // vectors
+    // vec2 metatable
+    luaL_newmetatable(lua, "vec2");
+    
+    lua_pushnumber(lua, 0);
+    lua_setfield(lua, -2, "x");
+
+    lua_pushnumber(lua, 0);
+    lua_setfield(lua, -2, "y");
+
+    lua_pushcfunction(lua, starry_vec2_str);
+    lua_setfield(lua, -2, "__tostring");
+
+    lua_pop(lua, 1);
+
+    // vec2 static
     lua_createtable(lua, 0, 1);
+
     lua_pushcfunction(lua, starry_vec2_new);
     lua_setfield(lua, -2, "new");
+
     lua_setglobal(lua, "vec2");
 }
 
@@ -157,5 +152,7 @@ void StExtensions_free()
 
 void StExtensions_run(const char* path)
 {
-    luaL_dofile(lua, path);
+    if (luaL_dofile(lua, path)) {
+        fprintf(stderr, "[EXTENSIONS] Lua error: %s\n", lua_tostring(lua, -1));
+    }
 }
