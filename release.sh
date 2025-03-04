3bad2c43af8d7c60bd8867f856ac32e979391f03
hellory4n <hellory4n@gmail.com>
2025-03-04 13:48:25 -0500

    i love windows

--- release.sh
+++ release.sh
@@ -11,6 +11,8 @@ echo "Compiling to Windows..."
 mkdir stellarthing-windows
 mkdir stellarthing-windows/assets
 cp -r assets/* stellarthing-windows/assets
+# i love windows
+cp vendor/raylib.dll stellarthing-windows/raylib.dll
 env GOOS=windows GOARCH=amd64 go build -o stellarthing-windows/stellarthing.exe
 zip -r stellarthing-windows.zip stellarthing-windows
 
