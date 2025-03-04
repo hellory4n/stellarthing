3133c47779575939fd1022f44b5edfd0d58d8ab2
hellory4n <hellory4n@gmail.com>
2025-03-03 22:52:12 -0500

    :)

---
+++ release.sh
@@ -0,0 +1,17 @@
+echo "Compiling to Linux..."
+
+mkdir stellarthing-linux
+mkdir stellarthing-linux/assets
+cp -r assets/* stellarthing-linux/assets
+env GOOS=linux GOARCH=amd64 go build -o stellarthing-linux/stellarthing
+tar -czvf stellarthing-linux.tar.gz stellarthing-linux
+
+echo "Compiling to Windows..."
+
+mkdir stellarthing-windows
+mkdir stellarthing-windows/assets
+cp -r assets/* stellarthing-windows/assets
+env GOOS=windows GOARCH=amd64 go build -o stellarthing-windows/stellarthing.exe
+zip -r stellarthing-windows.zip stellarthing-windows
+
+echo "Compiling completed."
