4400325eac5c2436ab89704c93cfec9c5a54cbc7
hellory4n <hellory4n@gmail.com>
2025-03-05 20:23:05 -0500

    1.0.0-alpha

--- core/core.go
+++ core/core.go
@@ -11,16 +11,16 @@ import (
 const Debug = true
 
 // engine version string
-const StarryVersionStr = "v4.0.0"
+const StarryVersionStr = "v4.0.0-dev"
 
 // engine version
 var StarryVersion Vec3i = NewVec3i(4, 0, 0)
 
 // game version string
-const GameVersionStr = "v0.11.0"
+const GameVersionStr = "v1.0.0-alpha"
 
 // game version
-var GameVersion Vec3i = NewVec3i(0, 11, 0)
+var GameVersion Vec3i = NewVec3i(1, 0, 0)
 
 // yeah
 const GameName = "stellarthing"
