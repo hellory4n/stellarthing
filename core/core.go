ad98351a38a56f3392a686b528c09bd378b9a3ba
hellory4n <hellory4n@gmail.com>
2025-03-16 12:05:09 -0400

    1.1.0

--- core/core.go
+++ core/core.go
@@ -11,16 +11,16 @@ import (
 const Debug = true
 
 // engine version string
-const StarryVersionStr = "v4.0.0-dev"
+const StarryVersionStr = "v4.1.0-dev"
 
 // engine version
-var StarryVersion Vec3i = Vec3i{4, 0, 0}
+var StarryVersion Vec3i = Vec3i{4, 1, 0}
 
 // game version string
-const GameVersionStr = "v1.0.0-alpha"
+const GameVersionStr = "v1.1.0-alpha"
 
 // game version
-var GameVersion Vec3i = Vec3i{1, 0, 0}
+var GameVersion Vec3i = Vec3i{1, 1, 0}
 
 // yeah
 const GameName = "stellarthing"
